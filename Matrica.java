95bdd54d34d37c5218efce64dc37cb57f673ce96
rbrezic <brezicrafael10@gmail.com>
2020-07-29 14:34:34 +0200

    Matrica s popunjavanjem u smjeru kazaljke na satu - final

--- Matrica.java
+++ Matrica.java
@@ -10,34 +10,34 @@ public class Matrica {
 		s=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj stupaca:")); // unosenje stupaca
 		
 		int[][] matrica= new int[r][s];
-		int k=1, stupac1=0, stupac2=s-1, red1=0, red2=r-1; 			//k = elementi od 1 pa nadalje
-		 while(k<=r*s)
+		int k=1, ls=0, ds=s-1, gr=0, dr=r-1; 			//k = elementi od 1 pa nadalje // "-1" zbog toga sto krecem od 0
+		 while(k<=r*s)									//ukupan broj elemenata																
 	        {
-			 
-			 	for(i=stupac2-1;i>=stupac1+1;i--) 
-	            {
-	                matrica[red2][i]=k++;
+			 											//ls = lijevi stupci u matrici
+			 	for(i=ds;i>=ls;i--) 					//ds = desni stupci u matrici				
+	            {										//gr = gornji redovi u matrici
+	                matrica[dr][i]=k++;					//dr = donji redovi u matrici
 	            } 
-	            for(j=red2;j>=red1;j--) 
+	            for(j=dr-1;j>=gr;j--)  									
 	            {
-	                matrica[j][stupac1]=k++; 
+	                matrica[j][ls]=k++; 
 	            }
-	            for(i=stupac1+1;i<=stupac2;i++)
+	            for(i=ls+1;i<=ds;i++) 								
 	            {
-	                matrica[red1][i]=k++; 
+	                matrica[gr][i]=k++; 
 	            }
 
-	            for(j=red1+1;j<=red2;j++) 
+	            for(j=gr+1;j<=dr-1;j++)  									
 	            {
-	                matrica[j][stupac2]=k++; 
+	                matrica[j][ds]=k++; 
 	            }
 
 	            
-	            
-	            stupac1++;
-	            red1++;
-	            stupac2--;
-	            red2--;
+	            dr--;		//u smjeru
+	            ls++; 		//kazaljke 
+	            gr++;    	//na
+	            ds--;  		// satu
+	            				
 	        }
 		 
 		 
